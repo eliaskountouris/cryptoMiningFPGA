582a22dd0adeae15d243747dadb5e6810d5ad5d0
Elias K <eliaskountouris9@gmail.com>
2024-09-16 10:09:42 -0400

    cleanup

--- verif/sha256/sim_build/Vtop__Dpi.d
+++
@@ -1,9 +0,0 @@
-Vtop__Dpi.o: Vtop__Dpi.cpp Vtop__Dpi.h \
- /usr/share/verilator/include/vltstd/svdpi.h Vtop.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h \
- /usr/share/verilator/include/verilated_cov.h \
- /usr/share/verilator/include/verilated.h
--- verif/sha256/sim_build/Vtop__Syms.d
+++
@@ -1,9 +0,0 @@
-Vtop__Syms.o: Vtop__Syms.cpp Vtop__Syms.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h Vtop.h \
- /usr/share/verilator/include/verilated_cov.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/vltstd/svdpi.h Vtop___024root.h
